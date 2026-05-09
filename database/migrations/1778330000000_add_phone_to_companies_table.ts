54ac216670ba5c4b47501d66682ab1c3a38c038c
Abel KISUELELE <abeltenda@gmail.com>
2026-05-09 21:23:24 +0100

    Fix duplicate phone migration

--- database/migrations/1778330000000_add_phone_to_companies_table.ts
+++ database/migrations/1778330000000_add_phone_to_companies_table.ts
@@ -4,12 +4,24 @@ export default class extends BaseSchema {
   protected tableName = 'companies'
 
   async up() {
+    const hasPhoneColumn = await this.schema.hasColumn(this.tableName, 'phone')
+
+    if (hasPhoneColumn) {
+      return
+    }
+
     this.schema.alterTable(this.tableName, (table) => {
       table.string('phone').nullable()
     })
   }
 
   async down() {
+    const hasPhoneColumn = await this.schema.hasColumn(this.tableName, 'phone')
+
+    if (!hasPhoneColumn) {
+      return
+    }
+
     this.schema.alterTable(this.tableName, (table) => {
       table.dropColumn('phone')
     })
